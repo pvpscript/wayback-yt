d25c4a4bafd548ee073eb5546b2e08fd89c599a9
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-05-12 02:22:08 -0300

    feat: working example. Needs refactoring!

--- yt_back/core/main.py
+++ yt_back/core/main.py
@@ -2,31 +2,51 @@ import sys
 import csv
 import yt_dlp
 import asyncio
+import time
+
+from yt_dlp.utils import DownloadError
+from typing import Optional, Mapping, Any
 
 from yt_back.wayback.cdx_iterator import CDXIterator
 from yt_back.wayback.cdx_fetcher import CDXFetcher
 
-def archive_url_fmt(timestamp, url):
+
+def archive_url_fmt(timestamp: str, url: str) -> str:
     return f'https://web.archive.org/web/{timestamp}/{url}'
 
-async def task(orig, url):
+def check_url(url: str) -> bool:
+    # Suppress the yt_dlp logger here
+    # Add log for attempt
     with yt_dlp.YoutubeDL() as dl:
-        info = dl.extract_info(url, download=False)
-        return orig, info.get('format')
+        try:
+            info = dl.extract_info(url, download=False)
+            return info not in (None, {})
+        except DownloadError as exp:
+            pass
+
+    return False
+
+async def filter_url(cdx: CDXFetcher) -> str:
+    for data in await cdx.fetch():
+        iterator = CDXIterator(data)
+
+        for row in csv.DictReader(iterator, delimiter=iterator.delimiter):
+            archive_url = archive_url_fmt(
+                timestamp=row['timestamp'],
+                url=row['original'],
+            )
+
+            # Maybe skip the test and download it at once
+            # If that's the case, there will be no need to suppress yt_dlp's logger
+            if check_url(archive_url):
+                yield archive_url
+                break
 
 async def main():
     urls = sys.argv[1:]
 
-    async with CDXFetcher(urls) as f:
-        for r in await f.fetch():
-            iterator = CDXIterator(r)
+    async with CDXFetcher(urls) as cdx:
+        archived_urls = [a_url async for a_url in filter_url(cdx)]
 
-            tasks = []
-            for row in csv.DictReader(iterator, delimiter=iterator.delimiter):
-                #print(row['timestamp'], row['statuscode'])
-                original = row['original']
-                archive_url = archive_url_fmt(row['timestamp'], original)
-                tasks.append(asyncio.ensure_future(task(original, archive_url)))
-
-            for i in await asyncio.gather(*tasks):
-                print(i)
+        with yt_dlp.YoutubeDL() as ydl:
+            ydl.download(archived_urls)
